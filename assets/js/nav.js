be6973b80ddf4523e4cc387a5d8943d40b695db4
Minidu-Sathsara <138673580+Minidu-Sathsara@users.noreply.github.com>
2025-08-12 23:58:14 +0530

    Remove unnecessary comments in scroll event handler for navbar visibility

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,14 +5,14 @@ window.addEventListener('scroll', function () {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   
   if (scrollTop > lastScrollTop) {
-    // Scrolling down - hide navbar
-    navbar.style.top = '-80px';  // Adjust height based on your navbar height
+   
+    navbar.style.top = '-80px';  
     navbar.style.transition = 'top 0.3s';
   } else {
-    // Scrolling up - show navbar
+   
     navbar.style.top = '0';
     navbar.style.transition = 'top 0.3s';
   }
   
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
 });
